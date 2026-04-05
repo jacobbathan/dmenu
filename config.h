cad8a8999f0f9ec65376f63047c55bfb8ed7d7cd
jacobbathan <jacobbathan@gmail.com>
2026-04-04 18:39:17 -0700

    added centering

--- config.h
+++ config.h
@@ -1,20 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;
+static int min_width = 500;
+static const float menu_height_ratio =
+    4.0f; /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=12"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"monospace:size=12",
+                              "Iosevka Nerd Font Mono:size=14"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#bbbbbb", "#222222"},
+    [SchemeSel] = {"#eeeeee", "#005577"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
